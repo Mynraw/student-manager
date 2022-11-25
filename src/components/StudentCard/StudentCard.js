4b0bf602dc99b259746274c904ee4e5a60c0bde5
Arya Acikgoz <aryaacikgoz@outlook.com>
2022-11-25 13:45:45 +0300

    changes

--- src/components/StudentCard/StudentCard.js
+++ src/components/StudentCard/StudentCard.js
@@ -1,20 +1,26 @@
 import React from "react";
+import PersonImg from "../../images/person-img.png";
 
 const StudentCard = ({ student }) => {
   return (
-    <div className="student-info">
-      <p>
-        Student: <span>{student.studentInput}</span>
-      </p>
-      <p>
-        Course: <span>{student.studentCourseInput}</span>
-      </p>
-      <p>
-        Instructor: <span>{student.studentInstructorInput}</span>
-      </p>
-      <p>
-        Score: <span>{student.studentScoreInput}</span>
-      </p>
+    <div className="student-card">
+      <div className="img-container">
+        <img src={PersonImg} alt="person" />
+      </div>
+      <div className="student-info">
+        <p>
+          Student: <span>{student.studentInput}</span>
+        </p>
+        <p>
+          Course: <span>{student.studentCourseInput}</span>
+        </p>
+        <p>
+          Instructor: <span>{student.studentInstructorInput}</span>
+        </p>
+        <p>
+          Score: <span>{student.studentScoreInput}</span>
+        </p>
+      </div>
     </div>
   );
 };
